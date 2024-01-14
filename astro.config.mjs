fcd74461a8c0ac073cf1028f46fd77f68a414e27
Termux-Master93 <brisantur8034@gmail.com>
2024-01-13 22:27:26 -0500

    add i18n catalan

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,14 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'ca'],
+    routing: {
+      prefixDefaultLocale: false, // el español seria la raiz (/)  y el catalan seria /ca
+
+    }
+
+  }
 });
